603831856951e4683cd932b4fa953c3647582fd3
RemanAlwadiee <riman.alwadiee03@gmail.com>
2024-11-04 16:36:27 +0300

    Initial Commit

---
+++ SaneApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SaneApp
+//
+//  Created by Reman Alwadiee on 02/05/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!hi")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SaneApp/SaneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaneAppApp.swift
+//  SaneApp
+//
+//  Created by Reman Alwadiee on 02/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SaneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
